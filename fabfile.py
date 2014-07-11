98cf98b59fb1e52b200aedc29c95855171a52fb4
villancikos <villancikos@gmail.com>
2014-07-10 20:01:16 -0500

    Testing Fabric

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import local
+
+# prepare for deployment
+
+def test():
+    local("python test_tasks.py -v && python test_users.py -v")
+
+def commit():
+    message = raw_input("Enter a git commit message: ")
+    local("git add . && git commit -am '{}'".format(message))
+
+def push():
+    local("git push origin master")
+
+def prepare():
+    #test()
+    commit()
+    push()
